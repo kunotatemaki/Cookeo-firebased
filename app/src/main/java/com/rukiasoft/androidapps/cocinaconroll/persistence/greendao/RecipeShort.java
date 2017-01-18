84de8944ab10a5b81d0579b2e1b2a0b594d29101
Kuno Tatemaki <raulfeliz@gmail.com>
2017-01-18 17:19:15 +0100

    posible introducción de errores en el salvado de ingredientes y procedimiento. revisar

--- app/src/main/java/com/rukiasoft/androidapps/cocinaconroll/persistence/greendao/RecipeShort.java
+++ app/src/main/java/com/rukiasoft/androidapps/cocinaconroll/persistence/greendao/RecipeShort.java
@@ -29,17 +29,20 @@ public class RecipeShort {
     private String picture;
     private Boolean vegetarian;
     private Boolean favourite;
+    private String tip;
+    private Integer minutes;
+    private Integer portions;
     @NotNull
     private Long timestamp;
     @NotNull
     private Boolean downloadRecipe = false;
     private Boolean downloadPicture;
-/** Used to resolve relations */
-@Generated(hash = 2040040024)
-private transient DaoSession daoSession;
-/** Used for active entity operations. */
-@Generated(hash = 1756051443)
-private transient RecipeShortDao myDao;
+    /** Used to resolve relations */
+    @Generated(hash = 2040040024)
+    private transient DaoSession daoSession;
+    /** Used for active entity operations. */
+    @Generated(hash = 1756051443)
+    private transient RecipeShortDao myDao;
 
     public RecipeShort() {
     }
@@ -70,153 +73,180 @@ private transient RecipeShortDao myDao;
         this.timestamp = System.currentTimeMillis();
     }
 
-@Generated(hash = 619197396)
-public RecipeShort(Long id, @NotNull String key, String name, String type,
-        Integer icon, String picture, Boolean vegetarian, Boolean favourite,
-        @NotNull Long timestamp, @NotNull Boolean downloadRecipe,
-        Boolean downloadPicture) {
-    this.id = id;
-    this.key = key;
-    this.name = name;
-    this.type = type;
-    this.icon = icon;
-    this.picture = picture;
-    this.vegetarian = vegetarian;
-    this.favourite = favourite;
-    this.timestamp = timestamp;
-    this.downloadRecipe = downloadRecipe;
-    this.downloadPicture = downloadPicture;
-}
-
-public Long getId() {
-    return this.id;
-}
-
-public void setId(Long id) {
-    this.id = id;
-}
-
-public String getKey() {
-    return this.key;
-}
-
-public void setKey(String key) {
-    this.key = key;
-}
-
-public String getName() {
-    return this.name;
-}
-
-public void setName(String name) {
-    this.name = name;
-}
-
-public String getType() {
-    return this.type;
-}
-
-public void setType(String type) {
-    this.type = type;
-}
-
-public Integer getIcon() {
-    return this.icon;
-}
-
-public void setIcon(Integer icon) {
-    this.icon = icon;
-}
-
-public String getPicture() {
-    return this.picture;
-}
-
-public void setPicture(String picture) {
-    this.picture = picture;
-}
-
-public Boolean getVegetarian() {
-    return this.vegetarian;
-}
-
-public void setVegetarian(Boolean vegetarian) {
-    this.vegetarian = vegetarian;
-}
-
-public Boolean getFavourite() {
-    return this.favourite;
-}
-
-public void setFavourite(Boolean favourite) {
-    this.favourite = favourite;
-}
-
-public Long getTimestamp() {
-    return this.timestamp;
-}
-
-public void setTimestamp(Long timestamp) {
-    this.timestamp = timestamp;
-}
-
-public Boolean getDownloadRecipe() {
-    return this.downloadRecipe;
-}
-
-public void setDownloadRecipe(Boolean downloadRecipe) {
-    this.downloadRecipe = downloadRecipe;
-}
-
-public Boolean getDownloadPicture() {
-    return this.downloadPicture;
-}
-
-public void setDownloadPicture(Boolean downloadPicture) {
-    this.downloadPicture = downloadPicture;
-}
-
-/**
- * Convenient call for {@link org.greenrobot.greendao.AbstractDao#delete(Object)}.
- * Entity must attached to an entity context.
- */
-@Generated(hash = 128553479)
-public void delete() {
-    if (myDao == null) {
-        throw new DaoException("Entity is detached from DAO context");
+    @Generated(hash = 1002064868)
+    public RecipeShort(Long id, @NotNull String key, String name, String type, Integer icon, String picture,
+            Boolean vegetarian, Boolean favourite, String tip, Integer minutes, Integer portions, @NotNull Long timestamp,
+            @NotNull Boolean downloadRecipe, Boolean downloadPicture) {
+        this.id = id;
+        this.key = key;
+        this.name = name;
+        this.type = type;
+        this.icon = icon;
+        this.picture = picture;
+        this.vegetarian = vegetarian;
+        this.favourite = favourite;
+        this.tip = tip;
+        this.minutes = minutes;
+        this.portions = portions;
+        this.timestamp = timestamp;
+        this.downloadRecipe = downloadRecipe;
+        this.downloadPicture = downloadPicture;
     }
-    myDao.delete(this);
-}
 
-/**
- * Convenient call for {@link org.greenrobot.greendao.AbstractDao#refresh(Object)}.
- * Entity must attached to an entity context.
- */
-@Generated(hash = 1942392019)
-public void refresh() {
-    if (myDao == null) {
-        throw new DaoException("Entity is detached from DAO context");
+    public Long getId() {
+        return this.id;
     }
-    myDao.refresh(this);
-}
 
-/**
- * Convenient call for {@link org.greenrobot.greendao.AbstractDao#update(Object)}.
- * Entity must attached to an entity context.
- */
-@Generated(hash = 713229351)
-public void update() {
-    if (myDao == null) {
-        throw new DaoException("Entity is detached from DAO context");
+    public void setId(Long id) {
+        this.id = id;
     }
-    myDao.update(this);
-}
 
-/** called by internal mechanisms, do not call yourself. */
-@Generated(hash = 1159410144)
-public void __setDaoSession(DaoSession daoSession) {
-    this.daoSession = daoSession;
-    myDao = daoSession != null ? daoSession.getRecipeShortDao() : null;
-}
+    public String getKey() {
+        return this.key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getIcon() {
+        return this.icon;
+    }
+
+    public void setIcon(Integer icon) {
+        this.icon = icon;
+    }
+
+    public String getPicture() {
+        return this.picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public Boolean getVegetarian() {
+        return this.vegetarian;
+    }
+
+    public void setVegetarian(Boolean vegetarian) {
+        this.vegetarian = vegetarian;
+    }
+
+    public Boolean getFavourite() {
+        return this.favourite;
+    }
+
+    public void setFavourite(Boolean favourite) {
+        this.favourite = favourite;
+    }
+
+    public String getTip() {
+        return this.tip;
+    }
+
+    public void setTip(String tip) {
+        this.tip = tip;
+    }
+
+    public Integer getMinutes() {
+        return this.minutes;
+    }
+
+    public void setMinutes(Integer minutes) {
+        this.minutes = minutes;
+    }
+
+    public Integer getPortions() {
+        return this.portions;
+    }
+
+    public void setPortions(Integer portions) {
+        this.portions = portions;
+    }
+
+    public Long getTimestamp() {
+        return this.timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Boolean getDownloadRecipe() {
+        return this.downloadRecipe;
+    }
+
+    public void setDownloadRecipe(Boolean downloadRecipe) {
+        this.downloadRecipe = downloadRecipe;
+    }
+
+    public Boolean getDownloadPicture() {
+        return this.downloadPicture;
+    }
+
+    public void setDownloadPicture(Boolean downloadPicture) {
+        this.downloadPicture = downloadPicture;
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#delete(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 128553479)
+    public void delete() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.delete(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#refresh(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 1942392019)
+    public void refresh() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.refresh(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#update(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 713229351)
+    public void update() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.update(this);
+    }
+
+    /** called by internal mechanisms, do not call yourself. */
+    @Generated(hash = 1159410144)
+    public void __setDaoSession(DaoSession daoSession) {
+        this.daoSession = daoSession;
+        myDao = daoSession != null ? daoSession.getRecipeShortDao() : null;
+    }
+
 
 }
