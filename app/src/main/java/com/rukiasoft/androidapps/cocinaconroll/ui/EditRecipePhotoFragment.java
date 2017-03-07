51485b5f709ceb97c61901577f91ac85342f4008
Raúl Feliz Alonso <iRoll@MacBook-Air-de-Raul.local>
2017-03-07 02:36:28 +0100

    pics from gallery stage 1

--- app/src/main/java/com/rukiasoft/androidapps/cocinaconroll/ui/EditRecipePhotoFragment.java
+++ app/src/main/java/com/rukiasoft/androidapps/cocinaconroll/ui/EditRecipePhotoFragment.java
@@ -365,11 +365,11 @@ public class EditRecipePhotoFragment extends Fragment {
     }
 
     private Uri getUri() {
-        String state = Environment.getExternalStorageState();
-        if(!state.equalsIgnoreCase(Environment.MEDIA_MOUNTED))
+        //String state = Environment.getExternalStorageState();
+        //if(!state.equalsIgnoreCase(Environment.MEDIA_MOUNTED))
             return MediaStore.Images.Media.INTERNAL_CONTENT_URI;
 
-        return MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        //return MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
     }
 
 
